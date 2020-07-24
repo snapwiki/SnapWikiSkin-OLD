d923a4a5b4d75285b41922aef3f99530c81d3846
GrahamSH-LLK <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-07-24 10:10:03 -0400

    Update SnapWikiSkin.php

--- SnapWikiSkin.php
+++ SnapWikiSkin.php
@@ -1,1 +1,15 @@
+<?php
+/**
+ * SnapWikiSkin
+ *
+ * @file
+ * @ingroup Skins
+ * @author (multiple)
+ * @license MIT
+ */
 
+if ( !function_exists( 'wfLoadSkin' ) ) {
+	die( 'This skin has been upgraded and now requires MediaWiki 1.25 or newer.' );
+}
+
+wfLoadSkin( 'SnapWikiSkin' );
